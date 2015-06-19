65c9e4d6be13b1aa9d06dddaf55a345c11228454
John McParland <john.mcparland@cgi.com>
2015-06-19 12:04:42 +0100

    another attempt at building the front end - it works, but I'm not sure it built everything.  Also added noddy script to find usages of third party libs

--- localFrontEndBuild.sh
+++ localFrontEndBuild.sh
@@ -2,7 +2,7 @@
 ###############################################################################
 ## This is to be executed on your local machine and builds the web/war module
 ###############################################################################
-MAVEN_WEB_PROFILES="grunt-unix,web-war,web-war-with-gpw,web-war-with-ui-plugins clean package"
+MAVEN_WEB_PROFILES="web-war,web-war-with-gpw,web-war-with-ui-plugins"
 MAVEN_ARGS="-X"
 MAVEN_OPTS="-Xms1024m -Xmx2048m -XX:MaxPermSize=512m -XX:+HeapDumpOnOutOfMemoryError"
 
@@ -11,5 +11,5 @@ echo "MAVEN_ARGS = ${MAVEN_ARGS}"
 echo "MAVEN_GOALS = ${MAVEN_GOALS}"
 
 export MAVEN_OPTS
-mvn "${MAVEN_ARGS}" -P "${MAVEN_WEB_PROFILES}" -pl web-war -amd clean package install -DskipTests=true -Dsource.skip=true
+mvn "${MAVEN_ARGS}" -P "${MAVEN_WEB_PROFILES}" -pl web/war -amd clean package install -DskipTests=true -Dsource.skip=true
 
